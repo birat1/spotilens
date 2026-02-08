19b2e4da4f00416569ce1db5bcef11f04353ba21
ba01081 <93601583+birat1@users.noreply.github.com>
2026-02-08 12:20:31 +0000

    updated logout

--- frontend/spotilens/services/auth.ts
+++ frontend/spotilens/services/auth.ts
@@ -5,6 +5,7 @@ export async function login() {
 }
 
 export function logout() {
-  localStorage.removeItem('spotify_token');
+  localStorage.removeItem('spotify_access_token');
+  localStorage.removeItem('spotify_refresh_token');
   window.location.href = '/';
 }
