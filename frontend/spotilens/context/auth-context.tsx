de30c48a777463fdb2ba66b9a41cb97d69809c35
ba01081 <93601583+birat1@users.noreply.github.com>
2026-02-08 00:51:39 +0000

    Removed the redirection from auth context

--- frontend/spotilens/context/auth-context.tsx
+++ frontend/spotilens/context/auth-context.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
 import {
   createContext,
   ReactNode,
@@ -29,25 +28,14 @@ const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<UserProfile | null>(null);
   const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  const pathname = usePathname();
 
   useEffect(() => {
     fetch(`${BACKEND_URL}/api/me/profile`, { credentials: 'include' })
       .then((res) => (res.ok ? res.json() : null))
-      .then((data) => {
-        setUser(data);
-        // If user is not authenticated and is trying to access a protected route, redirect to home
-        if (!data && pathname != '/') {
-          router.push('/');
-        }
-      })
-      .catch(() => {
-        setUser(null);
-        if (pathname != '/') router.push('/');
-      })
+      .then((data) => setUser(data))
+      .catch(() => setUser(null))
       .finally(() => setLoading(false));
-  }, [pathname, router]);
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, loading }}>
